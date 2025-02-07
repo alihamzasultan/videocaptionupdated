cd6539bf70679ae0eba461176196941c5337c9f7
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-02-07 17:45:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import whisper
 import subprocess
 import os
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageColor  # Added ImageColor
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageColor  # AddedImageColor
 from tqdm import tqdm
 import streamlit as st
 import tempfile
